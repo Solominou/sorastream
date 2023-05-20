e51d694514bef5d355c4c73b0662fe2e89e978f5
solominou <96089086+Solominou@users.noreply.github.com>
2023-05-20 09:58:57 +0300

    Add files via upload

---
+++ SoraStream/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+// use an integer for version numbers
+version = 131
+
+android {
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+
+        buildConfigField("String", "SORA_API", "\"${properties.getProperty("SORA_API")}\"")
+        buildConfigField("String", "SORAHE", "\"${properties.getProperty("SORAHE")}\"")
+        buildConfigField("String", "SORAXA", "\"${properties.getProperty("SORAXA")}\"")
+        buildConfigField("String", "SORATED", "\"${properties.getProperty("SORATED")}\"")
+
+
+    }
+}
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+     description = "#1 best extention based on MultiAPI"
+     authors = listOf("Hexated", "Sora")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Anime",
+        "Movie",
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/hexated/cloudstream-extensions-hexated/master/SoraStream/Icon.png"
+}
